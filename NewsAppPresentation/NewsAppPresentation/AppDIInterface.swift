6ef641873270cd4b06513eab0e32e725ebcdea10
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-06-09 11:01:30 +0200

    adding AppDIInterface

---
+++ NewsAppPresentation/NewsAppPresentation/AppDIInterface.swift
@@ -0,0 +1,12 @@
+//
+//  AppDIInterface.swift
+//  NewsAppPresentation
+//
+//  Created by Mohammed Abd El-Aty on 09/06/2021.
+//
+
+import Foundation
+
+public protocol AppDIInterface {
+    func newsSearchDependencies() -> NewsSearchVM
+}
