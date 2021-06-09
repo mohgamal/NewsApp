863fdb15d7a4df9710953188acd63d12aea2ac0e
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-06-09 11:05:42 +0200

    updating main app folder structure and add app environment

---
+++ NewsApp/NewsApp/AppEnvironment/AppEnvironment.swift
@@ -0,0 +1,12 @@
+//
+//  AppEnvironment.swift
+//  NewsApp
+//
+//  Created by Mohammed Abd El-Aty on 09/06/2021.
+//
+
+import Foundation
+
+class AppEnvironment {
+    let baseURL = "https://newsapi.org/v2/everything?apiKey=b6d869a966e04668a27027095b1864e6&"
+}
