79a30ae4326834bcd4634862cee2825915ebd2b9
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-06-09 09:12:13 +0200

    adding NewRemoteDataSource with implementation to fetch remote data

---
+++ NewsAppData/NewsAppData/News/NewsRemoteDataSource.swift
@@ -0,0 +1,64 @@
+//
+//  NewsRemoteDataSource.swift
+//  NewsAppData
+//
+//  Created by Mohammed Abd El-Aty on 09/06/2021.
+//
+
+import NewsAppDomain
+
+public protocol NewsRemoteDataSourceInterface {
+    init (urlString: String)
+    
+    func searchForNews(by filter: String?, page: Int?, handler: @escaping (Result<NewsModel, NewsAppError>) -> Void)
+}
+
+public class NewRemoteDataSource: NewsRemoteDataSourceInterface {
+    let urlString: String
+    
+    required public init(urlString: String ) {
+        self.urlString = urlString
+    }
+    
+    public func searchForNews(by filter: String?, page: Int?, handler: @escaping (Result<NewsModel, NewsAppError>) -> Void) {
+        guard let url = URL(string: "\(urlString)&q=\(filter ?? "")&page=\(page ?? 1)") else {
+            handler(.failure(NewsAppError.NotFound))
+            return
+        }
+        
+        let task  = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                switch httpResponse.statusCode {
+                case 200...299:
+                    break
+                case 400:
+                    handler(.failure(NewsAppError.BadRequest))
+                case 404:
+                    handler(.failure(NewsAppError.NotFound))
+                case 500:
+                    handler(.failure(NewsAppError.InternalServerError))
+                default:
+                    handler(.failure(NewsAppError.InternalServerError))
+                }
+             }
+            
+            guard let data = data else {
+              if let error = error {
+                handler(.failure(NewsAppError.UnknownError(message: error.localizedDescription)))
+              }
+              return
+            }
+
+            let decoder = JSONDecoder()
+            
+            do {
+                let result = try decoder.decode(NewsModel.self, from: data)
+                handler(.success(result))
+            } catch {
+                handler(.failure(NewsAppError.WrongData))
+            }
+        }
+        task.resume()
+    }
+}
