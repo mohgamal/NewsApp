c74cc8e45c0def0a0b5dc31c41f26711ad17f47e
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-06-09 11:11:15 +0200

    update news search vc extension access level

--- NewsAppPresentation/NewsAppPresentation/Scenes/NewsSearchScene/NewsSearchVC+TableView.swift
+++ NewsAppPresentation/NewsAppPresentation/Scenes/NewsSearchScene/NewsSearchVC+TableView.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 extension NewsSearchVC: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.newsSearchVM.newsResultEntity.articles?.count ?? 0
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ArticleCell.cellIdentifier, for: indexPath) as! ArticleCell
         cell.configureCell(with: self.newsSearchVM.newsResultEntity.articles?[indexPath.row])
         return cell
