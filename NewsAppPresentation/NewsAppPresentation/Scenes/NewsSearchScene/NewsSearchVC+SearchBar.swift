cefc31bec55a46bca64de0d62150bf965c2601c7
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-06-09 11:02:58 +0200

    adding NewsSearchVC search bar and tableview extensions

---
+++ NewsAppPresentation/NewsAppPresentation/Scenes/NewsSearchScene/NewsSearchVC+SearchBar.swift
@@ -0,0 +1,23 @@
+//
+//  NewsSearchView+SearchBar.swift
+//  NewsAppPresentation
+//
+//  Created by Mohammed Abd El-Aty on 09/06/2021.
+//
+
+import UIKit
+
+
+extension NewsSearchVC: UISearchBarDelegate {
+    public func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchtext = searchBar.text else { return }
+        
+        if searchtext != "" {
+            self.addSpinner()
+        }
+        
+        self.newsSearchVM.getNewsSearchResult(with: searchtext, page: 1)
+        
+        self.searchBar.resignFirstResponder()
+    }
+}
---
+++ NewsAppPresentation/NewsAppPresentation/Scenes/NewsSearchScene/NewsSearchVC+TableView.swift
@@ -0,0 +1,20 @@
+//
+//  NewsSearchView+TableView.swift
+//  NewsAppPresentation
+//
+//  Created by Mohammed Abd El-Aty on 09/06/2021.
+//
+
+import UIKit
+
+extension NewsSearchVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.newsSearchVM.newsResultEntity.articles?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ArticleCell.cellIdentifier, for: indexPath) as! ArticleCell
+        cell.configureCell(with: self.newsSearchVM.newsResultEntity.articles?[indexPath.row])
+        return cell
+    }
+}
